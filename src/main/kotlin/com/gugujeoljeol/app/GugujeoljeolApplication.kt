0d1af3299739347958347b5dfdbb1bab554d582b
socar-ornn <ornn@socar.kr>
2022-07-22 19:27:15 +0900

    first commit

---
+++ src/main/kotlin/com/gugujeoljeol/app/GugujeoljeolApplication.kt
@@ -0,0 +1,11 @@
+package com.gugujeoljeol.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GugujeoljeolApplication
+
+fun main(args: Array<String>) {
+	runApplication<GugujeoljeolApplication>(*args)
+}
---
+++ src/test/kotlin/com/gugujeoljeol/app/GugujeoljeolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gugujeoljeol.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GugujeoljeolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
